b5ca65652dd5ea9eefa2a208772d6f8132c2f42c
Andrew Boreham <pt-snoop@hotmail.co.uk>
2012-03-22 21:21:42 +0000

    first headers for gui/game scope, as well as the view and gooey superclasses

--- src/gui/game/game.hh
+++ src/gui/game/game.hh
@@ -1,1 +1,32 @@
- 
+#include "irrlicht/irrlicht.h"
+
+#include "irrlichtreceiver.hh"
+#include "texturepack.hh"
+
+#include "cell.hh"
+#include "world.hh"
+
+#ifndef GAME_HH
+#define GAME_HH
+
+namespace game{
+
+  class Game {
+  public:
+    Game::Game ();
+    void load();
+    void update();
+    IrrlichtReceiver* receiver ;
+
+  private:
+    Cell* cell ;
+    World* cell ;
+    CellGooey* cellFace ;
+    WorldGooey* worldFace ;
+    MainMenuGooey* mainMenuFace ;
+    TexturePack* texPack ;
+  };
+
+}
+
+#endif
--- src/gui/game/texturepack.hh
+++ src/gui/game/texturepack.hh
@@ -1,1 +1,27 @@
- 
+#include "irrlicht/irrlicht.h"
+#include <map>
+#include <string>
+
+#ifndef TEXTUREPACK_HH
+#define TEXTUREPACK_HH
+
+namespace game {
+
+  using namespace irr;
+
+  class TexturePack {
+  public:
+    video::ITexture* getTexture ( string ) ;
+    video::SColorf* getColour ( string ) ;
+    void addTexture ( string , ITexture* ) ;
+    void addColour ( string , SColorf* ) ;
+    void readFile ( string ) ;
+    
+  private:
+    map< string ; video::ITexture* > textureBank ;
+    map< string ; video::SColorf* > colourBank ;
+  };
+
+}
+
+#endif
